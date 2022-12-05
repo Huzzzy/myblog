2ee26d69003b552cc5ce1ffcfd9b3b6e7b0eee52
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-12-05 14:51:49 +0300

    Внедрение зависимости через конструктор

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -1,28 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostsController extends AbstractController
 {
-    #[Route('/posts', name: 'app_posts')]
-    public function index(): Response
+    /** @var PostRepository $postRepository */
+    private $postRepository;
+
+    public function __construct(PostRepository $postRepository)
     {
-        $posts = [
-            'post_1' => [
-                'title' => 'Заголовок первого поста',
-                'body' => 'Тело первого поста'
-            ],
-            'post_2' => [
-                'title' => 'Заголовок второго поста',
-                'body' => 'Тело второго поста'
-            ]
-        ];
-        return $this->render('posts/index.html.twig', [
-            'posts' => $posts,
-        ]);
+        $this->postRepository = $postRepository;
     }
 }
