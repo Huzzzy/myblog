1bd572eb21e095120cda0465f72acdcf492af2d0
IlyaSushkevich <ilya.sushkevich@gmail.com>
2022-12-06 12:15:52 +0300

    Добавление экшена для создания статей через форму

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -5,18 +5,24 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\PostType;
 use App\Repository\PostRepository;
+use Cocur\Slugify\Slugify;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostsController extends AbstractController
 {
     /** @var PostRepository $postRepository */
     private $postRepository;
+    private $doctrine;
 
-    public function __construct(PostRepository $postRepository)
+    public function __construct(PostRepository $postRepository, ManagerRegistry $doctrine)
     {
         $this->postRepository = $postRepository;
+        $this->doctrine = $doctrine;
     }
 
     /**
@@ -28,6 +34,30 @@ class PostsController extends AbstractController
 
         return $this->render('posts/index.html.twig', [
             'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/posts/new", name="new_blog_post")
+     */
+    public function addPost(Request $request, Slugify $slugify)
+    {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post->setSlug($slugify->slugify($post->getTitle()));
+            $post->setCreatedAt(new \DateTime());
+
+            $em = $this->doctrine->getManager();
+            $em->persist($post);
+            $em->flush();
+
+            return $this->redirectToRoute('blog_posts');
+        }
+        return $this->render('posts/new.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 
